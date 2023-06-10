3792c267bee333edd0a87d43f973e5dfb67fa6c0
swathivetri <swathivetri1992@gmail.com>
2023-06-10 13:53:02 +0530

    readytosend

---
+++ app.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const express = require('express');
+var cors = require('cors')
+
+const app = express();
+const dotenv = require('dotenv');
+
+// get config vars
+dotenv.config();
+
+const sequelize = require('./util/database');
+const User = require('./models/users');
+
+
+const userRoutes = require('./routes/user')
+
+
+
+
+
+app.use(cors());
+
+// app.use(bodyParser.urlencoded());  ////this is for handling forms
+app.use(express.json());  //this is for handling jsons
+
+app.use('/user', userRoutes)
+
+
+//app.use((req, res) => {
+  //  console.log('urlll', req.url);
+//res.sendFile(path.join(__dirname, 'Login/login.html'));
+//})
+
+
+
+sequelize.sync()
+    .then(() => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+
